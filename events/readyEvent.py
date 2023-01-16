1bd77f83fdd3aa878933504267635b1b2473c4a3
fancydsDEV <122800683+fancydsDEV@users.noreply.github.com>
2023-01-16 17:58:01 +0400

    Update readyEvent.py

--- events/readyEvent.py
+++ events/readyEvent.py
@@ -1,19 +1,14 @@
 import disnake, config
 from disnake.ext import commands
-from pymongo import MongoClient
 
 class readyEvent(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.cluster = MongoClient(config.mongo_uri)
-        self.db = self.cluster["botdb"]
-        self.collection = self.db["economy"]
     
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'---------------------')
         print(f'БОТ: {self.bot.user}')
-
 
 def setup(bot):
     bot.add_cog(readyEvent(bot))
