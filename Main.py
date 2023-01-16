0a00417d06d79e3de8e031c25e0d5e94fa2c0075
fancydsDEV <122800683+fancydsDEV@users.noreply.github.com>
2023-01-16 17:53:24 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
-import disnake
+import disnake, os
 from disnake.ext import commands
 
 bot = commands.Bot(command_prefix = '.', intents = disnake.Intents.all(), help_command = None)
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):                        
+        bot.load_extension(f'commands.{filename[:-3]}')
+        print(f'Команда: {filename[:-3]} загружена.')
+
+for filename in os.listdir('./events'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'events.{filename[:-3]}')
+        print(f'Ивент: {filename[:-3]} загружен.')
 
 bot.run(config.TOKEN)
